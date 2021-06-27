9a1b0909f059d8b7143949165ea8458f021813e2
Mike Alien <cranzit@gmail.com>
2021-06-27 19:33:23 +0200

    hw5 is done

--- HomeWork5/project/js/main.js
+++ HomeWork5/project/js/main.js
@@ -1,4 +1,49 @@
-const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses'
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        catalogUrl: '/catalogData.json',
+        products: [],
+        filtered: [],
+        imgCatalog: 'https://via.placeholder.com/200x150',
+        userSearch: '',
+        show: false,
+    },
+    methods: {
+        getJson(url) {
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+        addProduct(product) {
+            console.log(product.id_product);
+        },
+        filter() {
+            const regexp = new RegExp(this.userSearch, 'i')
+            this.filtered = this.products.filter(product => regexp.test(product.product_name))
+        }
+    },
+    mounted() {
+        this.getJson(`${API + this.catalogUrl}`)
+            .then(data => {
+                for (let item of data) {
+                    this.products.push(item)
+                    this.filtered.push(item)
+                }
+            })
+        this.getJson(`getProducts.json`)
+            .then(data => {
+                for (let item of data) {
+                    this.products.push(item)
+                    this.filtered.push(item)
+                }
+            })
+    }
+})
+
 
 
 
